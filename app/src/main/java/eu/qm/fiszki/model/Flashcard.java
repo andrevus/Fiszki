5998813fe3b9f65395500d367543e537449fe684
Demat <matudan1991@gmail.com>
2016-02-06 19:46:12 +0100

    New DB using ORMLite: how to use:
            private Helper dbHelper;
            private RuntimeExceptionDao<Flashcard, Integer> flashcardDao;
            private RuntimeExceptionDao<Category, Integer> categoryDao;
            private List<Flashcard> flashcardList;
            private List<Category> categoryList;
    
            dbHelper = OpenHelperManager.getHelper(this, Helper.class);
            flashcardDao = dbHelper.getFlashcardDao();
            categoryDao = dbHelper.getCategoryDao();
    
            Category newCategory = new Category(1, "animals");
            Flashcard newCard = new Flashcard(1, "kot", "cat", 1, newCategory);
    
            categoryDao.create(newCategory);
            flashcardDao.create(newCard);
    
            flashcardList = flashcardDao.queryForAll();
            categoryList = categoryDao.queryForAll();
    
            OpenHelperManager.releaseHelper();

---
+++ app/src/main/java/eu/qm/fiszki/model/Flashcard.java
@@ -0,0 +1,72 @@
+package eu.qm.fiszki.model;
+
+import com.j256.ormlite.field.DatabaseField;
+
+import java.io.Serializable;
+
+public class Flashcard implements Serializable {
+
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField
+    private String word;
+    @DatabaseField
+    private String translation;
+    @DatabaseField
+    private int priority;
+    @DatabaseField(foreign = true)
+    private Category category;
+
+    public Flashcard() {}
+
+    public Flashcard(int id, String word, String translation, int priority, Category category) {
+        this.id = id;
+        this.word = word;
+        this.translation = translation;
+        this.priority = priority;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+
+    public void setTranslation(String translation) {
+        this.translation = translation;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
+
+
+
